2f5a28943826adeb356c5dc0365a08ec8ca00029
ASTRE DIVIN <astredivin19@gmail.com>
2026-06-21 14:18:24 +0000

    Nettoyage finale inchallah

--- app.py
+++ app.py
@@ -39,5 +39,6 @@ print("Debut......")
 def index():
     return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
