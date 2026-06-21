7d6df11a26180cd08e0bf789351472f5397212c3
ASTRE DIVIN <astredivin19@gmail.com>
2026-06-21 08:34:33 +0000

    Nettoyage finale inchallah

--- bdsql.py
+++ bdsql.py
@@ -16,7 +16,7 @@ host = os.environ.get("DB_HOST")
 port = os.environ.get("DB_PORT")
 db_name = os.environ.get("DB_NAME")
 
-DATABASE_URL = f"postgresql://{user}:{password}@{host}:{port}/{name}"
+DATABASE_URL = f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
 
 engine = create_engine(DATABASE_URL)
 
