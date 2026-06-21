bed4dbee8b10a90ba4d7f750ea1908fb29a81c43
ASTRE DIVIN <astredivin19@gmail.com>
2026-06-21 13:17:06 +0000

    Nettoyage finale inchallah

--- app.py
+++ app.py
@@ -10,13 +10,13 @@ load_dotenv()
 app.secret_key = os.environ.get("SECRET_KEY")
 app.config["MAX_CONTENT_LENGTH"] = 20 * 1024 * 1024
 """app.config["UPLOAD_FOLDER"] = "uploads/rapports" """
-app.config['MAIL_SERVER'] = 'smtp-relay.brevo.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] =True
+app.config['MAIL_SERVER'] = os.environ.get('MAIL_SERVER')
+app.config['MAIL_PORT'] = os.environ.get('MAIL_PORT')
+app.config['MAIL_USE_TLS'] =os.environ.get('MAIL_USE_TLS')
 app.config['MAIL_USE_SSL'] = False 
-app.config['MAIL_USERNAME'] = 'af70c8001@smtp-brevo.com'
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
-app.config['MAIL_DEFAULT_SENDER'] = 'folmusalfred@gmail.com'
+app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_PASSWORD')
 mail = Mail(app)
 
 cloudinary.config(
