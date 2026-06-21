6e9b70dabc971ea221aba8a3268ff8014f7f725c
ASTRE DIVIN <astredivin19@gmail.com>
2026-06-21 08:13:49 +0000

    Nettoyage finale et config

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_mail import Mail
 from flask_cors import CORS
 import cloudinary,cloudinary.uploader
 from dotenv import load_dotenv
+import os
 app = Flask(__name__)
 CORS(app)
 load_dotenv()
